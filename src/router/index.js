14ae1ee9c07c81df56dd5f10117129d89cda24d1
sitdikov <lissen2me@yandex.ru>
2019-12-14 16:33:29 +0300

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,22 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
   },
+  {
+    path: '/contacts',
+    name: 'contacts',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "contacts" */ '../views/Contacts.vue'),
+  },
+  {
+    path: '/test',
+    name: 'test',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "contacts" */ '../views/Test.vue'),
+  },
 ];
 
 const router = new VueRouter({
---
+++ src/views/Contacts.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Contacts</h1>
+</template>
