78cb4699936e81f5ee19534961357723a141ca4b
Sitdzikau Ihar <ihar.sitdzikau@yandex.by>
2020-05-24 21:23:48 +0300

    removed unused file;

--- src/main.js
+++ src/main.js
@@ -3,17 +3,14 @@ import VueSimpleAlert from 'vue-simple-alert';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import api from './api';
 import vuetify from './plugins/vuetify';
 
 Vue.use(VueSimpleAlert);
 
 Vue.config.productionTip = false;
-Vue.use(api);
 new Vue({
   router,
   store,
-  api,
   vuetify,
   render: h => h(App),
 }).$mount('#app');
